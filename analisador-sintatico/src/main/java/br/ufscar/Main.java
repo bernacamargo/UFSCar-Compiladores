18c1141d98ed92ecd5869ad9b3edf0bf76725f55
Paulo Betareli <paulobetarelI@hotmail.com>
2021-03-13 15:34:06 -0300

    adicionado leitura de arquivo e token parsers

--- analisador-sintatico/src/main/java/br/ufscar/Main.java
+++ analisador-sintatico/src/main/java/br/ufscar/Main.java
@@ -5,6 +5,15 @@
  */
 package br.ufscar;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
+
 /**
  *
  * @author paulo
@@ -14,8 +23,22 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-       
+     public static void main(String args[]) {
+        CharStream cs;
+        try {
+            cs = CharStreams.fromFileName(args[0]);
+            AlLexer lex = new AlLexer(cs);
+            
+            FileWriter myWriter = new FileWriter(args[1]);
+            Token t = null;
+            
+            CommonTokenStream tokens = new CommonTokenStream(lex);
+            AlParser parser = new AlParser(tokens);
 
+            
+        } 
+        catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
