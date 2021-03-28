45ed465289a660f4e2d57bc233fae0acd8542036
RenataPriasler <42523994+RenataPraisler@users.noreply.github.com>
2021-03-28 20:47:49 -0300

    comentario sobre a classe

--- analisador-semanticoo/src/main/java/br/ufscar/EntradaTabelaDeSimbolos.java
+++ analisador-semanticoo/src/main/java/br/ufscar/EntradaTabelaDeSimbolos.java
@@ -5,6 +5,7 @@ public class EntradaTabelaDeSimbolos {
     public TipoAl tipo;
     public boolean ponteiro;
 
+    //Para conseguir encontrar o tipo e o nome da variavel e se é ponteiro ou não
     public EntradaTabelaDeSimbolos(String nome, TipoAl tipo, boolean ponteiro) {
         this.nome = nome;
         this.tipo = tipo;
